d697c70164428ada629a8b701cc55f553a67316d
Bright Udoette <brightudoette77@gmail.com>
2024-01-07 20:59:50 +0100

    add javascript function to chnage the article-1 content when the navigators are clicked

--- scripts/script.js
+++ scripts/script.js
@@ -48,7 +48,9 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       imageCount = 0;
     }
+    updateArticleContent;
     bannerContainer.style.backgroundImage = `url("${imageArray[imageCount]}")`;
+    updateArticleContent(imageCount);
     console.log(imageCount);
   });
 
@@ -59,8 +61,38 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       imageCount = lastArrayElement;
     }
+
     bannerContainer.style.backgroundImage = `url("${imageArray[imageCount]}")`;
+    updateArticleContent(imageCount);
     console.log(imageCount);
   });
   // logic to change the content of the text when the navigators are clicked
+  const articleHeader = document.querySelector(".article-1-header");
+  articleParagraph = document.querySelector(".article-1-text");
+  function updateArticleContent(element) {
+    if (element == 0) {
+      articleHeader.textContent = "Discover innovative ways to decorate";
+      articleParagraph.textContent = ` We provide unmatched quality, comfort, and style for property owners
+      across the country. Our experts combine form and function in bringing
+      your vision to life. Create a room in your own style with our collection
+      and make your property a reflection of you and what you love.`;
+    }
+    if (element == 1) {
+      articleHeader.textContent = `We are available all across the globe`;
+      articleParagraph.textContent = ` With stores all over the world, it's easy for you to find furniture for
+      your home or place of business. Locally, we’re in most major cities
+      throughout the country. Find the branch nearest you using our store
+      locator. Any questions? Don't hesitate to contact us today.`;
+    }
+
+    if (element == 2) {
+      articleHeader.textContent = `Manufactured with the best materials`;
+      articleParagraph.textContent = ` Our multifunctional collection blends design and function to suit your
+      individual taste. Make each room unique, or pick a cohesive theme that
+      best express your interests and what inspires you. Find the furniture
+      pieces you need, from traditional to contemporary styles or anything in
+      between. Product specialists are available to help you create your dream
+      space.`;
+    }
+  }
 });
